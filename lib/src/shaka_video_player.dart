65534ec781cae8ab1cdeeb2f2ac45a19c598eacb
Sairam Vemula <62637789+SairamVemula@users.noreply.github.com>
2023-10-27 16:08:38 +0530

    track change intantly

--- lib/src/shaka_video_player.dart
+++ lib/src/shaka_video_player.dart
@@ -297,7 +297,7 @@ class ShakaVideoPlayer extends VideoElementPlayer {
   @override
   Future<void> setTrackParameters(int? width, int? height, int? bitrate) async {
     shaka.Track? track = _trackDecision(width, height, bitrate);
-    if (track != null) _player.selectVariantTrack(track);
+    if (track != null) _player.selectVariantTrack(track, true, 0);
   }
 
   @override
